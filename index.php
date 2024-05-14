2a752ce46105dedec2d0f3253a88a3935f6d4f66
DanPhan <phamdangquang79@gmail.com>
2024-05-14 15:36:21 +0700

    index.php

--- index.php
+++ index.php
@@ -2,44 +2,46 @@
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Blood Donation</title>
-  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+  <link rel="stylesheet" href="style.css">
 </head>
 <body>
-  <div class="container">
+<main>
     <header>
-      <h1>Health Care </h1>
-      <nav class="navbar navbar-expand-sm bg-dark navbar-dark">
-        <ul class="navbar-nav">
-          <li class="nav-item"><a class="nav-link" href="#">Home</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">About</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">How to donate?</a></li>
-          <li class="nav-item"><a class="nav-link" href="#">Contact</a></li>
-          <button class="btnSignin-popup">Signin</button>
-        </ul>
-      </nav>
+      <h1 class="container">Health</h1>
+        <nav class="navbar">
+          <a href="#">Home</a>
+          <a href="#">About</a>
+          <a href="#">How to donate?</a>
+          <a href="#">Contact</a>
+          <button class="btnSignin-popup">Sign in</button>
+      </nav> 
     </header>
-    <main>
-      <div class="row">
-        <div class="col-sm-6">
-          <h2>Blood stocks are low! We urgently need more donors to give blood</h2> <li><a href="#">Blood type</a></li>
-          <p></p>
-        </div>
-        <div class="col-sm-6">
-          <h2>About Blood</h2>
-          <ul>
-            <li><a href="#">Benefits of blood donation</a></li>
-            <li><a href="#">Blood Donation Process</a></li>
-            <li><a href="#">Blood Shortage</a></li>
-          </ul>
-        </div>
+
+    <script src="script.js"></script>
+  </body>
+  <main>
+    <div class="row">
+      <div class="col-sm-6">
+        <h2>Blood stocks are low! We urgently need more donors to give blood</h2> <li><a href="#">Blood type</a></li>
+        <p></p>
       </div>
-    </main>
+      <div class="col-sm-6">
+        <h2>About Blood</h2>
+        <ul>
+          <li><a href="#">Benefits of blood donation</a></li>
+          <li><a href="#">Blood Donation Process</a></li>
+          <li><a href="#">Blood Shortage</a></li>
+        </ul>
+      </div>
+    </div>
+  </main>
 
-    <footer class="page-footer">
-      <p>&copy; 2024 Blood Donate Website</p> <li><a href="#">Contact us</a></li>
-    </footer>
-  </div>
+  <footer class="page-footer">
+    <p>&copy; 2024 Blood Donate Website</p> <li><a href="#">Contact us</a></li>
+  </footer>
+</div>
 
-  <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.
+<script src="https://cdn.jsdelivr.net/npm/jquery@3.5.
