6f56649481c0694f50965a59524fb41df3aaea92
champa1n73 <giakhuong0703@gmail.com>
2024-05-21 23:46:57 +0700

    Before merging from remote dev

--- createAccount.php
+++ createAccount.php
@@ -118,9 +118,55 @@ include('connect/connect.php');
     <!-- Testing NavBar End -->
 
     <div>
-    <a class="create-account" href="createAccount.php">Create Account</a>
+    <a class="create-account" href="">Create Account</a>
     </div>
 
+    <!-- Form -->
+    <div class="col-lg-9 wow fadeInUp createAccountForm" data-wow-delay="0.5s">
+                    <div class="bg-light rounded h-100 d-flex align-items-center p-5">
+                        <form>
+                            <div class="row g-3">
+                                <div class="col-12 col-sm-6">
+                                    <input type="text" class="form-control border-0" placeholder="Full Name" style="height: 55px;">
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <input type="email" class="form-control border-0" placeholder="Username" style="height: 55px;">
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <input type="text" class="form-control border-0" placeholder="Password" style="height: 55px;">
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <select class="form-select border-0" style="height: 55px;">
+                                        <option selected>Choose Account Type</option>
+                                        <option value="1">Admin</option>
+                                        <option value="2">Hospital</option>
+                                        <option value="3">Examinor</option>
+                                    </select>
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <div class="date" id="date" data-target-input="nearest">
+                                        <input type="text"
+                                            class="form-control border-0 datetimepicker-input"
+                                            placeholder="Choose Date" data-target="#date" data-toggle="datetimepicker" style="height: 55px;">
+                                    </div>
+                                </div>
+                                <div class="col-12 col-sm-6">
+                                    <div class="time" id="time" data-target-input="nearest">
+                                        <input type="text"
+                                            class="form-control border-0 datetimepicker-input"
+                                            placeholder="Choose Date" data-target="#time" data-toggle="datetimepicker" style="height: 55px;">
+                                    </div>
+                                </div>
+                                <div class="col-12">
+                                    <textarea class="form-control border-0" rows="5" placeholder="Describe your problem"></textarea>
+                                </div>
+                                <div class="col-12">
+                                    <button class="btn btn-primary w-100 py-3" type="submit">Book Appointment</button>
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
 
     <!-- Child Class Start -->
     <div class = "container my-5">
