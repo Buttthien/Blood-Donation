747a2753f03c59bdc1740c56db1e54c729b98dd1
champa1n73 <giakhuong0703@gmail.com>
2024-06-02 11:59:22 +0700

    fix connect with 404

--- connect/connect.php
+++ connect/connect.php
@@ -2,7 +2,7 @@
 
 if ( $_SERVER['REQUEST_METHOD']=='GET' && realpath(__FILE__) == realpath( $_SERVER['SCRIPT_FILENAME'] ) ) {
     header( 'HTTP/1.0 403 Forbidden', TRUE, 403 );
-    die( header( 'location: 404.php' ) );
+    die( header( 'location: /Blood-Donation/404.php' ) );
     }
 
 $host = "localhost";
